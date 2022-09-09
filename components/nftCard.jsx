6825b05f4a7ee077b237858d9d96c1e870441708
bigzp2022 <bigzp2022@outlook.com>
2022-09-10 00:47:17 +0800

    first commit

---
+++ components/nftCard.jsx
@@ -0,0 +1,17 @@
+export const NFTCard = ({ nft }) => {
+
+    return (
+        <div className="w-1/4 flex flex-col ">
+        <div className="rounded-md">
+            <img className="object-cover h-128 w-full rounded-t-md" src={nft.media[0].gateway} ></img>
+        </div>
+        <div className="flex flex-col y-gap-2 px-2 py-3 bg-slate-100 rounded-b-md h-110 ">
+            <div className="">
+                <h2 className="text-xl text-gray-800">{nft.title}</h2>
+            </div>
+
+        </div>
+
+    </div>
+    )
+}
